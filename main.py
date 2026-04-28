559ce93e8dce07bd74503db830c1a759c79a8d2f
hardwareowen-cmd <hardwareowen@gmail.com>
2026-04-28 11:43:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -303,4 +303,5 @@ def ComingSoon():
     return render_template("ComingSoon.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
